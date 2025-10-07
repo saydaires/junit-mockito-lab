70b6b345e4b193e273f2db7058c403d1d513981d
Sayd Aires <saydaires@gmail.com>
2025-10-06 23:32:49 -0300

    first test practiced

---
+++ meuPrimeiroTeste/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Calculadora {
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double diminuir(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        return a / b;
+    }
+
+}
---
+++ meuPrimeiroTeste/src/test/java/CalculadoraTest.java
@@ -0,0 +1,46 @@
+import org.example.Calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+    private Calculadora calculadora;
+
+    @BeforeEach
+    public void setUp() {
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void deveSomarDoisValores() {
+        double valorA = 10;
+        double valorB = 20;
+        double resultado = calculadora.somar(valorA, valorB);
+        Assertions.assertEquals(30, resultado);
+    }
+
+    @Test
+    public void deveSubtrairDoisValores() {
+        Calculadora calculadora = new Calculadora();
+        double valorA = 10;
+        double valorB = 20;
+        double resultado = calculadora.diminuir(valorA, valorB);
+        Assertions.assertEquals(-10, resultado);
+    }
+
+    @Test
+    public void deveMultiplicarDoisValores() {
+        double valorA = 10;
+        double valorB = 20;
+        double resultado = calculadora.multiplicar(valorA, valorB);
+        Assertions.assertEquals(200, resultado);
+    }
+
+    @Test
+    public void deveDividirDoisValores() {
+        double valorA = 10;
+        double valorB = 20;
+        double resultado = calculadora.dividir(valorA, valorB);
+        Assertions.assertEquals(0.5, resultado);
+    }
+}
